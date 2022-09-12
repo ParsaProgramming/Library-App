8d509675cb829f06899333338768f2f4d3dea059
Parsa <mohammadkazemi.parsa@gmail.com>
2022-09-12 17:28:59 +0430

    add validation

--- javascript/script.js
+++ javascript/script.js
@@ -6,27 +6,35 @@ let list = document.querySelector(".list");
 let table = document.querySelector("table");
 let tbody = document.querySelector("tbody");
 
-
 function AddBook(){
-    let tr = document.createElement("tr");
-    let td__1 = document.createElement("td");
-    td__1.innerHTML = bookName.value;
-    let td__2 = document.createElement("td");
-    td__2.innerHTML = bookAuthor.value;
-    let td__3 = document.createElement("td");
-    td__3.innerHTML = bookDate.value;
-    tr.appendChild(td__1);
-    tr.appendChild(td__2);
-    tr.appendChild(td__3);
-    tbody.appendChild(tr);
-    table.appendChild(tbody);
-    list.appendChild(table);
+    if(bookName.value == '' || bookDate.value == '' || bookAuthor.value == ''){
+        alert("please fillout those empty inputs");
+    }else{
+        let tr = document.createElement("tr");
+        let td__1 = document.createElement("td");
+        td__1.innerHTML = bookName.value;
+        let td__2 = document.createElement("td");
+        td__2.innerHTML = bookAuthor.value;
+        let td__3 = document.createElement("td");
+        td__3.innerHTML = bookDate.value;
+        tr.appendChild(td__1);
+        tr.appendChild(td__2);
+        tr.appendChild(td__3);
+        tbody.appendChild(tr);
+        table.appendChild(tbody);
+        list.appendChild(table);
+    
+        bookDate.value = '';
+        bookName.value = '';
+        bookAuthor.value = '';
+    
+        bookName.focus();
+    }
 
-    bookDate.value = '';
-    bookName.value = '';
-    bookAuthor.value = '';
+    
 
-    bookName.focus();
+    
+
 
 }
 
